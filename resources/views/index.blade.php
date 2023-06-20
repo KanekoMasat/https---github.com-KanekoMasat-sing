752984b1eb5fe060a580320451f6d420b3d09b91
masato <KanekoMasat@users.noreply.github.com>
2023-06-20 17:46:19 +0900

    add: lead of index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,10 +14,14 @@
     <div class="topSite-list-wrapper">
         <ul>
             <li class="topSite-list"><a href="{{ route('singing.create') }}" class="topSite-list-link">新規作成</a></li>
+            <span style="font-weight: bold">シートを作成します</span>
             <li class="topSite-list"><a href="{{ route('singing.index') }}" class="topSite-list-link">曲の編集</a></li>
+            <span style="font-weight: bold">作成したシートを編集します</span>
             <li class="topSite-list"><a href="{{ route('definition') }}" class="topSite-list-link">歌唱での様々な表現の紹介</a></li>
+            <span style="font-weight: bold">ビブラートやしゃくり等の技術はどういう技術なのか簡単に説明</span>
             <li class="topSite-list"><a href="{{ route('explanation') }}" class="topSite-list-link">使い方含め、このWebページについて</a>
             </li>
+            <span style="font-weight: bold">使い方解説</span>
         </ul>
     </div>
 
